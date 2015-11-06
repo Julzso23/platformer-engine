c55e2b4f3ffe1e6c65f08560962fe69d764abd21
Julian <julzso23@gmail.com>
2015-11-06 17:31:20 +0000

    Added framework config file

---
+++ platformer/config.lua
@@ -0,0 +1,8 @@
+local config = {}
+
+config.tileSize = 64 -- The width and height of each tile
+
+config.gravity = 1000 -- The gravitational acceleration (px/s^2) of any entities affected by gravity
+config.terminalVelocity = 400 -- The maximum vertical velocity
+
+return config
