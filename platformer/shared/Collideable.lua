12f7d7549e8442f5889a53672666cecf3f5138b2
Julian <julzso23@gmail.com>
2015-11-06 16:42:26 +0000

    Added collision world modification functions to Collideable

--- platformer/shared/Collideable.lua
+++ platformer/shared/Collideable.lua
@@ -10,8 +10,7 @@ function Collideable:initialize(width, height)
     Transformable.initialize(self)
     self.width = width
     self.height = height
-    local pos = self:getPosition()
-    collision.world:add(self, pos.x, pos.y, width, height)
+    self:worldAdd()
 end
 
 function Collideable:moveTo(x, y)
@@ -22,6 +21,20 @@ end
 function Collideable:move(x, y)
     local pos = self:getPosition()
     self:moveTo(pos.x + x, pos.y + y)
+end
+
+function Collideable:teleportTo(x, y)
+    self:setPosition(x, y)
+    collision.world:update(self, x, y)
+end
+
+function Collideable:worldAdd()
+    local pos = self:getPosition()
+    collision.world:add(self, pos.x, pos.y, self.width, self.height)
+end
+
+function Collideable:worldRemove()
+    collision.world:remove(self)
 end
 
 return Collideable
