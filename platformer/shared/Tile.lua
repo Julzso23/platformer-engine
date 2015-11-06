a36446534a3136f73081f5d868402aab3ce59a6d
Julian <julzso23@gmail.com>
2015-11-06 16:42:43 +0000

    Initialise tile with position

--- platformer/shared/Tile.lua
+++ platformer/shared/Tile.lua
@@ -4,8 +4,9 @@ local Collideable = include('shared.Collideable')
 
 local Tile = class('Tile', Collideable)
 
-function Tile:initialize()
+function Tile:initialize(x, y)
     Collideable.initialize(self, 64, 64)
+    self:teleportTo(x, y)
 end
 
 return Tile
