b86f2c452ec60fd2115b61901b3cc288786bab54
Julian <julzso23@gmail.com>
2015-11-12 00:46:29 +0000

    main.lua boots into main file for either game or editor, depending on launch parameter

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
 require('platformer.shared.helper')
+config = require('platformer.config')
+
+function love.load(args)
+    for k, v in pairs(args) do
+        if v == '--editor' then
+            require('platformer.editor.main')
+            love.load(args)
+            return
+        end
+    end
+    require('platformer.game.main')
+    love.load(args)
+end
