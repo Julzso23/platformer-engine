cf2192462a7e23d10130aae0ffb5dec4e6a40082
Julian <julzso23@gmail.com>
2015-11-12 12:55:17 +0000

    Added width and height to Tile class and converted initialiser parameters to grid units

--- platformer/shared/Tile.lua
+++ platformer/shared/Tile.lua
@@ -4,9 +4,9 @@ local Collideable = include('shared.Collideable')
 
 local Tile = class('Tile', Collideable)
 
-function Tile:initialize(x, y)
-    Collideable.initialize(self, 64, 64)
-    self:teleportTo(x, y)
+function Tile:initialize(x, y, w, h)
+    Collideable.initialize(self, w * config.tileSize, h * config.tileSize)
+    self:teleportTo(x * config.tileSize, y * config.tileSize)
 end
 
 return Tile
