7a888bbc67d6dc8f6ca4d76d53ad138dd3903517
Julian <julzso23@gmail.com>
2015-11-12 12:54:19 +0000

    Added ability to add and remove tiles to and from the world object

--- platformer/shared/World.lua
+++ platformer/shared/World.lua
@@ -1,9 +1,23 @@
 local class = include('middleclass')
 
+local Tile = include('shared.Tile')
+
 local World = class('World')
 
 function World:initialize()
     self.tiles = {}
+end
+
+function World:addTile(x, y, w, h)
+    table.insert(self.tiles, Tile:new(x, y, w, h))
+end
+
+function World:removeTile(x, y)
+    for k, v in pairs(self.tiles) do
+        if (x >= v.position.x) and (x < v.position.x + v.width) and (y >= v.position.y) and (y < v.position.y + v.height) then
+            table.remove(self.tiles, k)
+        end
+    end
 end
 
 return World
