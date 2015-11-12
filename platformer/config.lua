44fbe288d4c72dcfdfec7589ad9ffb13904bc2aa
Julian <julzso23@gmail.com>
2015-11-12 00:45:45 +0000

    Added game title option in config

--- platformer/config.lua
+++ platformer/config.lua
@@ -1,5 +1,7 @@
 local config = {}
 
+config.title = 'Example game' -- The title of the game
+
 config.tileSize = 64 -- The width and height of each tile
 
 config.gravity = 1000 -- The gravitational acceleration (px/s^2) of any entities affected by gravity
