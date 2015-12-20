e47d9439f156f2b2cb79e08f961a316e2c31705a
Julian <julzso23@gmail.com>
2015-12-20 12:49:46 +0000

    Added content from SaveGui into LoadGui

--- platformer/editor/LoadGui.lua
+++ platformer/editor/LoadGui.lua
@@ -3,19 +3,50 @@ local class = include('middleclass')
 local LoadGui = class('LoadGui')
 
 function LoadGui:initialize()
-    local loadButton = loveframes.Create('button')
-    loadButton:SetText('Load')
-    loadButton:SetX(80)
-    loadButton.OnClick = function()
-        world:load('test')
+    self.isClosed = true
+
+    local button = loveframes.Create('button')
+    button:SetPos(80, 0)
+    button:SetText('Load')
+    button.parentClass = self
+    button.OnClick = function(self)
+        self.parentClass:open()
     end
 
-    local loadButtonTooltip = loveframes.Create('tooltip')
-    loadButtonTooltip:SetObject(loadButton)
-    loadButtonTooltip:SetText('ctrl+o')
-    loadButtonTooltip:SetFollowCursor(false)
-    loadButtonTooltip:SetFollowObject(true)
-    loadButtonTooltip:SetOffsets(loadButton:GetWidth() / 4, loadButton:GetHeight())
+    local tooltip = loveframes.Create('tooltip')
+    tooltip:SetObject(button)
+    tooltip:SetText('ctrl+o')
+    tooltip:SetFollowCursor(false)
+    tooltip:SetFollowObject(true)
+    tooltip:SetOffsets(button:GetWidth() / 4, button:GetHeight())
+end
+
+function LoadGui:isOpen()
+    return not self.isClosed
+end
+
+function LoadGui:open()
+    self.isClosed = false
+
+    local frame = loveframes.Create('frame')
+    frame:SetName('Load map')
+    frame:SetDraggable(false)
+    frame:SetSize(256, 128)
+    frame:CenterWithinArea(0, 0, love.window.getWidth(), love.window.getHeight())
+    frame.parentClass = self
+    frame.OnClose = function(self)
+        self.parentClass.isClosed = true
+    end
+
+    local textBox = loveframes.Create('textinput', frame)
+    textBox:SetPos(0, 30)
+    textBox:SetWidth(frame:GetWidth())
+    textBox:SetPlaceholderText('Map name')
+    textBox.parentClass = self
+    textBox.OnEnter = function(self, text)
+        world:load(text)
+        self:GetParent():Remove()
+    end
 end
 
 return LoadGui
