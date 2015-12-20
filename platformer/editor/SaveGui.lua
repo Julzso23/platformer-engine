468ee432339305ae7bd91d1c54523c7dbd71c8f4
Julian <julzso23@gmail.com>
2015-12-20 00:20:46 +0000

    Started saving GUI

--- platformer/editor/SaveGui.lua
+++ platformer/editor/SaveGui.lua
@@ -3,10 +3,13 @@ local class = include('middleclass')
 local SaveGui = class('SaveGui')
 
 function SaveGui:initialize()
+    self.isClosed = false
+
     local saveButton = loveframes.Create('button')
     saveButton:SetText('Save')
-    saveButton.OnClick = function()
-        world:save('test')
+    saveButton.parentClass = self
+    saveButton.OnClick = function(self)
+        self.parentClass:open()
     end
 
     local saveButtonTooltip = loveframes.Create('tooltip')
@@ -15,6 +18,34 @@ function SaveGui:initialize()
     saveButtonTooltip:SetFollowCursor(false)
     saveButtonTooltip:SetFollowObject(true)
     saveButtonTooltip:SetOffsets(saveButton:GetWidth() / 4, saveButton:GetHeight())
+end
+
+function SaveGui:isOpen()
+    return self.isClosed
+end
+
+function SaveGui:open()
+    self.isClosed = true
+
+    local frame = loveframes.Create('frame')
+    frame:SetName('Save map')
+    frame:SetDraggable(false)
+    frame:SetSize(256, 128)
+    frame:CenterWithinArea(0, 0, love.window.getWidth(), love.window.getHeight())
+    frame.parentClass = self
+    frame.OnClose = function(self)
+        self.parentClass.isClosed = false
+    end
+
+    local textBox = loveframes.Create('textinput', frame)
+    textBox:SetPos(0, 30)
+    textBox:SetWidth(frame:GetWidth())
+    textBox:SetPlaceholderText('Map name')
+    textBox.parentClass = self
+    textBox.OnEnter = function(self, text)
+        world:save(text)
+        self.parentClass.isClosed = true
+    end
 end
 
 return SaveGui
