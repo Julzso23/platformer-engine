53eabf49f28d116ecec542b83909f7d1364bffee
Julian <julzso23@gmail.com>
2015-12-22 11:09:29 +0000

    Added assets folder to store images and audio

--- platformer/shared/resources.lua
+++ platformer/shared/resources.lua
@@ -5,11 +5,11 @@ resources.audio = {}
 
 function addResource(category, path, name)
     if category == 'images' then
-        resources.images[name] = love.graphics.newImage(path)
+        resources.images[name] = love.graphics.newImage('assets/images/' .. path)
     end
 
     if category == 'audio' then
-        resources.audio[name] = love.audio.newSource(path)
+        resources.audio[name] = love.audio.newSource('assets/audio/' .. path)
     end
 end
 
