2bd4590b57ec431b066300116a4d450ba22b5b4e
Julian <julzso23@gmail.com>
2015-12-22 17:31:23 +0000

    Added new textures to textures list

--- platformer/shared/textures.lua
+++ platformer/shared/textures.lua
@@ -2,9 +2,25 @@ local TileTexture = include('shared.TileTexture')
 
 local textures = {}
 
-textures.grass = TileTexture:new()
+textures.grass = TileTexture:new('grass')
 textures.grass:setBody('grassCenter.png')
 textures.grass:setSurface('grassMid.png')
+
+textures.dirt = TileTexture:new('dirt')
+textures.dirt:setBody('dirtCenter.png')
+textures.dirt:setSurface('dirtMid.png')
+
+textures.sand = TileTexture:new('sand')
+textures.sand:setBody('sandCenter.png')
+textures.sand:setSurface('sandMid.png')
+
+textures.snow = TileTexture:new('snow')
+textures.snow:setBody('snowCenter.png')
+textures.snow:setSurface('snowMid.png')
+
+textures.stone = TileTexture:new('stone')
+textures.stone:setBody('stoneCenter.png')
+textures.stone:setSurface('stoneMid.png')
 
 function textures.loadAll()
     for k, v in pairs(textures) do
