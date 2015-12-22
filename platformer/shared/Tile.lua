7662a3e4b4bec5815c8a62e5fa1c1d6af488e6d8
Julian <julzso23@gmail.com>
2015-12-22 11:15:28 +0000

    Added ability for tiles to have a separate surface and body texture

--- platformer/shared/Tile.lua
+++ platformer/shared/Tile.lua
@@ -4,18 +4,23 @@ local Collideable = include('shared.Collideable')
 
 local Tile = class('Tile', Collideable)
 
-function Tile:initialize(x, y, w, h, texture)
+function Tile:initialize(x, y, w, h, surfaceTexture, bodyTexture)
     Collideable.initialize(self, w * config.tileSize, h * config.tileSize)
     self:teleportTo(x * config.tileSize, y * config.tileSize)
 
-    self.texture = texture
+    self.surfaceTexture = surfaceTexture
+    self.bodyTexture = bodyTexture
 end
 
 function Tile:draw()
     love.graphics.setColor(255, 255, 255, 255)
     for x = 0, self.width / config.tileSize - 1 do
         for y = 0, self.height / config.tileSize - 1 do
-            love.graphics.draw(getResource('images', self.texture), self:getPosition().x + x * config.tileSize, self:getPosition().y + y * config.tileSize, 0, 0.5, 0.5)
+            if y == 0 then
+                love.graphics.draw(getResource('images', self.surfaceTexture), self:getPosition().x + x * config.tileSize, self:getPosition().y + y * config.tileSize, 0, 0.5, 0.5)
+            else
+                love.graphics.draw(getResource('images', self.bodyTexture), self:getPosition().x + x * config.tileSize, self:getPosition().y + y * config.tileSize, 0, 0.5, 0.5)
+            end
         end
     end
 end
